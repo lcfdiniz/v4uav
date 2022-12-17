13baee363ca129eb328a18d316ac06d4dd578237
lcfdiniz <lucas.diniz@engenharia.ufjf.br>
2022-12-17 11:50:10 -0300

    :pencil: Updating program interface

--- scripts/v4uav_interface.py
+++ scripts/v4uav_interface.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python
 import rospy
 from v4uav.msg import v4uav_command
+from v4uav.msg import v4uav_setpoint
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 import os
 
+class ToolbarApp(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        # Step 1: Frame configuration
+        self.parent = parent # MainApp frame
+        self.config(background='#ffffff')
+        logo_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'assets/v4uav_logo.png'))
+        self.logo = tk.PhotoImage(file=logo_path).subsample(10,10)
+        tk.Label(self, image=self.logo, bg='#ffffff').pack(side='left')
+        tk.Label(self, text='V4UAV', font=('TkDefaultFont', 14,'bold'), fg='#3e3e3e', bg='#ffffff').pack(side='left', pady=10, padx=10)
 class CommanderApp(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
         # Step 1: Frame configuration
-        tk.Label(self, text='Commander', font=('TkDefaultFont', 12,'bold'), fg='white', bg='#02577a').pack()
+        self.parent = parent # MainApp frame
+        tk.Label(self, text='Commander', font=('TkDefaultFont', 12,'bold'), fg='white', bg='#02577a').pack(pady=10)
         self.config(background='#02577a')
         self.n_inputs = {'TAKEOFF': 1, 'SET_POS': 4, 'SET_REL_POS': 4, 'HOLD': 0, 
                         'GET_POS': 0, 'TRACKING': 0, 'LANDING': 0, 'RTL': 0}
-        # Step 2: # Step 2: Organizing layout with (child) frames (inputs_box)
+        # Step 2: Structuring layout
         self.inputs_box = tk.Frame(self)
         self.inputs_box.config(background='#02577a')
         # Step 3: Adding widgets
@@ -34,7 +45,7 @@ class CommanderApp(tk.Frame):
         self.send_msg_btn = tk.Button(self.inputs_box, text='Send message', command=self.fill_msg)
         self.send_msg_btn.pack(pady=10)
         # Packing frame
-        self.inputs_box.pack(pady=50, fill='x')
+        self.inputs_box.pack(pady=10, fill='x')
     
     def enable_inputs(self, event):
         mode = self.combobox.get()
@@ -66,71 +77,103 @@ class CommanderApp(tk.Frame):
         msg.input_3 = self.command[3]
         msg.input_4 = self.command[4]
         rate = rospy.Rate(20)
-        v4uav_pub.publish(msg)
+        command_pub.publish(msg)
         rate.sleep()
 
 class ControllerApp(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         # Step 1: Frame configuration
-        self.parent = parent
-        self.label = tk.Label(self, text='Controller', font=('TkDefaultFont', 12,'bold'), fg='#3e3e3e', bg='#02a9f7').pack()
+        self.parent = parent # MainApp frame
+        tk.Label(self, text='Controller', font=('TkDefaultFont', 12,'bold'), fg='#3e3e3e', bg='#02a9f7').pack(pady=10)
         self.config(background='#02a9f7')
-        # Step 2: Organizing layout with (children) frames (top and bottom)
-        self.top = tk.Frame(self)
-        self.top.config(background='#02a9f7')
-        self.bottom = tk.Frame(self)
-        self.bottom.config(background='#02a9f7')
+        # Step 2: Structuring layout
+        # The Controller frame has 2 major portions: xy_frame (top) and zyaw_frame (bottom)
+        self.xy_frame = tk.Frame(self)
+        self.xy_frame.config(background='#02a9f7')
+        self.zyaw_frame = tk.Frame(self)
+        self.zyaw_frame.config(background='#02a9f7')
         # Step 3: Adding widgets
-        self.slider_x = tk.Scale(self.top, from_=1.0, to=-1.0, resolution=0.1, orient='vertical', label='X', font=('TkDefaultFont', 12,'bold'), fg='#3e3e3e', bg='#02a9f7', highlightthickness=0)
+        self.slider_x = tk.Scale(self.xy_frame, from_=1.0, to=-1.0, resolution=0.1, orient='vertical', 
+                                label='X', font=('TkDefaultFont', 12,'bold'), fg='#3e3e3e', bg='#02a9f7', highlightthickness=0, 
+                                command=self.send_msg)
         self.slider_x.grid(row=0, column=0, padx=30)
-        self.slider_y = tk.Scale(self.top, from_=-1.0, to=1.0, resolution=0.1, orient='horizontal', label='Y', font=('TkDefaultFont', 12,'bold'), fg='#3e3e3e', bg='#02a9f7', highlightthickness=0)
+        self.slider_y = tk.Scale(self.xy_frame, from_=-1.0, to=1.0, resolution=0.1, orient='horizontal', 
+                                label='Y', font=('TkDefaultFont', 12,'bold'), fg='#3e3e3e', bg='#02a9f7', highlightthickness=0,
+                                command=self.send_msg)
         self.slider_y.grid(row=0, column=1, padx=30)
-        self.slider_z = tk.Scale(self.bottom, from_=1.0, to=-1.0, resolution=0.1, orient='vertical', label='Z', font=('TkDefaultFont', 12,'bold'), fg='#3e3e3e', bg='#02a9f7', highlightthickness=0)
+        self.slider_z = tk.Scale(self.zyaw_frame, from_=1.0, to=-1.0, resolution=0.1, orient='vertical', 
+                                label='Z', font=('TkDefaultFont', 12,'bold'), fg='#3e3e3e', bg='#02a9f7', highlightthickness=0,
+                                command=self.send_msg)
         self.slider_z.grid(row=0, column=0, padx=30)
-        self.slider_yaw = tk.Scale(self.bottom, from_=-1.0, to=1.0, resolution=0.1, orient='horizontal', label='Yaw', font=('TkDefaultFont', 12,'bold'), fg='#3e3e3e', bg='#02a9f7', highlightthickness=0)
+        self.slider_yaw = tk.Scale(self.zyaw_frame, from_=-1.0, to=1.0, resolution=0.1, orient='horizontal',
+                                label='Yaw', font=('TkDefaultFont', 12,'bold'), fg='#3e3e3e', bg='#02a9f7', highlightthickness=0,
+                                command=self.send_msg)
         self.slider_yaw.grid(row=0, column=1, padx=30)
         # Packing frames
-        self.top.pack(pady=50)
-        self.bottom.pack()
-        # Step 3: Biding
-
+        self.xy_frame.pack(pady=20)
+        self.zyaw_frame.pack()
+    
+    def send_msg(self, event):
+        msg = v4uav_setpoint()
+        msg.vx_sp = self.slider_x.get()
+        msg.vy_sp = self.slider_y.get()
+        msg.vz_sp = self.slider_z.get()
+        msg.yaw_sp = self.slider_yaw.get()
+        rate = rospy.Rate(20)
+        input_pub.publish(msg)
+        rate.sleep()
 class MainApp(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
-        # Step 1: Root configuration
         tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
+        # Step 1: Root configuration
+        self.parent = parent # root
         self.parent.title('V4UAV')
         self.parent.geometry("800x400")
         # Step 2: Setting the icon
-        icon_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'assets/v4uav.png'))
-        icon = tk.PhotoImage(file=icon_path)
-        self.parent.tk.call('wm', 'iconphoto', self.parent._w, icon)
+        icon_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'assets/v4uav_icon.png'))
+        self.icon = tk.PhotoImage(file=icon_path)
+        self.parent.tk.call('wm', 'iconphoto', self.parent._w, self.icon)
         # Step 3: Creating the menu bar
         self.menubar = tk.Menu(self.parent)
-        # File menu
-        self.filemenu = tk.Menu(self.menubar, tearoff=0)
+        self.filemenu = tk.Menu(self.menubar, tearoff=0) # File menu
         self.filemenu.add_command(label='Close', command=exit)
         self.menubar.add_cascade(menu=self.filemenu, label='File')
-        # Help menu
-        self.helpmenu = tk.Menu(self.menubar, tearoff=0)
-        self.helpmenu.add_command(label='About', command=exit)
+        self.helpmenu = tk.Menu(self.menubar, tearoff=0) # Help menu
+        self.helpmenu.add_command(label='About', command=self.about)
         self.menubar.add_cascade(menu=self.helpmenu, label='Help')
-        # Configuring menubar
         self.parent.configure(menu=self.menubar)
-        # Step 4: Organizing layout with (children) frames (Commander and Controller)
+        # Step 4: Structuring layout
         # The interface has 2 major portions: Commander and Controller
         self.commander = CommanderApp(self)
         self.controller = ControllerApp(self)
+        # The toolbar only displays an image
+        self.toolbar = ToolbarApp(self)
         # Packing the frames
+        self.toolbar.pack(side='top', fill='x')
         self.commander.pack(side='left', fill='both', expand=True)
         self.controller.pack(side='left', fill='both', expand=True)
+    
+    def about(self):
+        win = tk.Toplevel()
+        win.title("About")
+        win.tk.call('wm', 'iconphoto', win._w, self.icon)
+        text = """V4UAV is a ROS package to control UAVs in 
+        power transmission line inspection task 
+        using computer vision techniques. 
+        It is the master thesis project of Lucas F. Diniz 
+        (lcfdiniz@outlook.com), also the developer and mantainer. 
+        For more information, please refer to the 
+        GitHub repo (https://github.com/lcfdiniz/V4UAV)."""
+        tk.Label(win, text=text).pack(padx=20, pady=20)
+        tk.Button(win, text='OK', command=win.destroy).pack(pady=10)
 
 def interface():
     rospy.init_node('v4uav_interface')
-    # Step 1: Publish to a MAVROS topic
-    global v4uav_pub
-    v4uav_pub = rospy.Publisher("/v4uav/command", v4uav_command, queue_size=10)
+    # Step 1: Publish to MAVROS topics
+    global command_pub, input_pub
+    command_pub = rospy.Publisher("/v4uav/command", v4uav_command, queue_size=10)
+    input_pub = rospy.Publisher("/v4uav/input", v4uav_setpoint, queue_size=10)
     # Step 2: Creating Tkinter interface
     root = tk.Tk()
     MainApp(root).pack(side="top", fill="both", expand=True)
